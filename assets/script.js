ba6e1bcff0ce1dfafd3dab7dea6be147f9ff0a40
JAG <jagrasso42@gmail.com>
2022-09-28 21:23:11 -0400

    small improvements to Rock, Paper, Sciccors function

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,8 @@ var playerWins = 0;
 
 var computerWins = 0;
 
+var ties = 0;
+
 var gamesPlayed = 0;
 
 function rockPaperSciccors() {
@@ -17,40 +19,47 @@ function rockPaperSciccors() {
 
   while (playerMove != "R" && playerMove != "P" && playerMove != "S" ) {
     playerMove = prompt("Enter 'R' for rock, 'P' for paper or 'S' for Scissors:");
+    playerMove = playerMove.toUpperCase();
+    if (playerMove != "R" && playerMove != "P" && playerMove != "S" ) {
+      alert('Invalid Input \nTry again')
+    }
   }
 
   if (playerMove === "R" && computerMove === "S") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ' You win!');
+    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', You win!');
     playerWins++;
     gamesPlayed++;
   } else if (playerMove === "R" && computerMove === "P") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ' Computer wins!');
+    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', Computer wins!');
     computerWins++;
     gamesPlayed++;
   } else if (playerMove === "R" && computerMove === "R") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ' It\'s a tie!');
+    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', It\'s a tie!');
+    ties++;
     gamesPlayed++;
   } else if (playerMove === "P" && computerMove === "S") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ' Computer wins!');
+    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', Computer wins!');
     computerWins++;
     gamesPlayed++;
   } else if (playerMove === "P" && computerMove === "R") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ' You win!');
+    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', You win!');
     playerWins++;
     gamesPlayed++;
   } else if (playerMove === "P" && computerMove === "P") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ' It\'s a tie!');
+    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', It\'s a tie!');
+    ties++;
     gamesPlayed++;
   } else if (playerMove === "S" && computerMove === "P") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ' You win!');
+    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', You win!');
     playerWins++;
     gamesPlayed++;
   } else if (playerMove === "S" && computerMove === "R") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ' Computer wins!');
+    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', Computer wins!');
     computerWins++;
     gamesPlayed++;
   } else {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ' It\'s a tie!');
+    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', It\'s a tie!');
+    ties++;
     gamesPlayed++;
   }
   
@@ -58,7 +67,7 @@ function rockPaperSciccors() {
 
 while (stillPlaying) {
   rockPaperSciccors();
-  alert('R, P, S record:\nPlayer: ' + playerWins + '\nComputer: ' + computerWins + '\nTotal Games: ' + gamesPlayed);
+  alert('R, P, S record:\nPlayer: ' + playerWins + '\nComputer: ' + computerWins + '\nTies: ' + ties + '\nTotal Games: ' + gamesPlayed);
   stillPlaying = confirm('Play again?');
 }
 
