0c6cde4e8738d4cf463143289094c7a0efb08d96
JAG <jagrasso42@gmail.com>
2022-09-29 12:14:11 -0400

    broke down program functionality into individual functions

---
+++ assets/pseudocode-example.js
@@ -0,0 +1,28 @@
+// Dependencies
+
+// Data
+  // array of possible moves
+  // variables to track wins, losses, ties and number of games played
+
+// Functions
+  //generate move for computer (r, p, or s)
+  //compare user move and computer move to decide who wins
+  //display result of game and record of wins and losses
+
+// User Interactions
+  //get move from user (r, p, or s)
+
+  //ask if user wants to play agian
+
+// Initialization
+  // call function to begin game
+
+
+// Pseudocode -
+  //start game
+  //get move from user (r, p, or s)
+  //generate move for computer (r, p, or s)
+  //compare user move and computer move to decide who wins
+  //display result of game and record of wins and losses
+  //ask if user wants to play again
+  //start new game or end program
--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,75 @@
+// Declarations
+var stillPlaying = true;
+var moves = ["R", "P", "S"];
+var playerWins = 0;
+var computerWins = 0;
+var ties = 0;
+var gamesPlayed = 0; 
+
+// User greeting
 alert("Welcome to Rock, Paper, Sciccors!");
 
-var stillPlaying = true;
-
-var moves = ["R", "P", "S"];
-
-var playerWins = 0;
-
-var computerWins = 0;
-
-var ties = 0;
-
-var gamesPlayed = 0;
-
-function rockPaperSciccors() {
-  var playerMove = "";
-  var computerMoveIndex = Math.floor(Math.random() * moves.length);
-  var computerMove = moves[computerMoveIndex];
-
-  while (playerMove != "R" && playerMove != "P" && playerMove != "S" ) {
-    playerMove = prompt("Enter 'R' for rock, 'P' for paper or 'S' for Scissors:");
-    playerMove = playerMove.toUpperCase();
-    if (playerMove != "R" && playerMove != "P" && playerMove != "S" ) {
-      alert('Invalid Input \nTry again')
+// Function to get and validate the player's move
+function getPlayerMove() {
+  var move = "";
+  while (move != "R" && move != "P" && move != "S" ) {
+    move = prompt("Enter 'R' for rock, 'P' for paper or 'S' for Scissors:");
+    move = move.toUpperCase();
+    if (move != "R" && move != "P" && move != "S") {
+      alert('Invalid Input\nTry again');
     }
   }
+  return move;
+}
 
-  if (playerMove === "R" && computerMove === "S") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', You win!');
+function getComputerMove() {
+  var moveIndex = Math.floor(Math.random() * moves.length);
+  var computerMove = moves[moveIndex];
+  return computerMove;
+}
+
+// Function takes arguments playerMove and computerMove from rockPaperScissors() and decides who wins
+function shoot(p, c) {
+  // user wins
+  if (
+    (p === "R" && c === "S") ||
+    (p === "P" && c === "R") ||
+    (p === "S" && c === "P")
+  ) {
+    alert('You played ' + p + ', the computer played ' + c + ', You win!');
     playerWins++;
     gamesPlayed++;
-  } else if (playerMove === "R" && computerMove === "P") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', Computer wins!');
+  //computer wins
+  } else if (
+    (p === "R" && c === "P") || 
+    (p === "P" && c === "S") || 
+    (p === "S" && c === "R")
+  ) {
+    alert('You played ' + p + ', the computer played ' + c + ', Computer wins!');
     computerWins++;
     gamesPlayed++;
-  } else if (playerMove === "R" && computerMove === "R") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', It\'s a tie!');
-    ties++;
-    gamesPlayed++;
-  } else if (playerMove === "P" && computerMove === "S") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', Computer wins!');
-    computerWins++;
-    gamesPlayed++;
-  } else if (playerMove === "P" && computerMove === "R") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', You win!');
-    playerWins++;
-    gamesPlayed++;
-  } else if (playerMove === "P" && computerMove === "P") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', It\'s a tie!');
-    ties++;
-    gamesPlayed++;
-  } else if (playerMove === "S" && computerMove === "P") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', You win!');
-    playerWins++;
-    gamesPlayed++;
-  } else if (playerMove === "S" && computerMove === "R") {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', Computer wins!');
-    computerWins++;
-    gamesPlayed++;
+  // tie
   } else {
-    alert('You played ' + playerMove + ', the computer played ' + computerMove + ', It\'s a tie!');
+    alert('You played ' + p + ', the computer played ' + c + ', It\'s a tie!');
     ties++;
     gamesPlayed++;
   }
-  
 }
 
+// Main game function
+function rockPaperSciccors() {
+  // Stores return value from getPlayerMove()
+  var playerMove = getPlayerMove();
+  // Stores return value from getComputerMove()
+  var computerMove = getComputerMove();
+  // Compares player move and computer move and decides winner
+  shoot(playerMove, computerMove);
+}
+
+// while loop calls the game function as long as the user chooses to continue
 while (stillPlaying) {
   rockPaperSciccors();
   alert('R, P, S record:\nPlayer: ' + playerWins + '\nComputer: ' + computerWins + '\nTies: ' + ties + '\nTotal Games: ' + gamesPlayed);
   stillPlaying = confirm('Play again?');
 }
-
 alert('bye!');
